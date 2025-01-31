af95e2eb9a2ae0d9ab354fba68d37dc23d59fb3b
ArisSpn_9 <arissopian0909@gmail.com>
2025-02-01 03:52:35 +0700

    change : style

--- lib/widgets/bottom_navbar.dart
+++ lib/widgets/bottom_navbar.dart
@@ -4,6 +4,7 @@ import '../pages/home_page.dart';
 import '../pages/whistlist_page.dart';
 import '../pages/favorite_page.dart';
 import '../pages/profile_page.dart';
+import '../data/colors.dart';
 
 class BottomNavBar extends StatefulWidget {
   const BottomNavBar({super.key});
@@ -32,29 +33,72 @@ class _BottomNavBarState extends State<BottomNavBar> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: _screens[_selectedIndex],
-      bottomNavigationBar: NavigationBar(
-        height: 70,
-        elevation: 0,
-        selectedIndex: _selectedIndex,
-        onDestinationSelected: _onItemTapped,
-        destinations: const [
-          NavigationDestination(
-            icon: Icon(Iconsax.home),
-            label: 'Home',
-          ),
-          NavigationDestination(
-            icon: Icon(Iconsax.save_2),
-            label: 'Whistlist',
-          ),
-          NavigationDestination(
-            icon: Icon(Iconsax.heart),
-            label: 'Favorite',
-          ),
-          NavigationDestination(
-            icon: Icon(Iconsax.user),
-            label: 'Profile',
-          ),
-        ],
+      extendBody: true,
+      bottomNavigationBar: Container(
+        height: 60,
+        margin: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: AppColors.accent,
+          borderRadius: BorderRadius.circular(25),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.3),
+              blurRadius: 10,
+              offset: const Offset(0, 5),
+            ),
+          ],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            _buildNavItem(0, Iconsax.home, 'Home'),
+            _buildNavItem(1, Iconsax.save_2, 'Whistlist'),
+            _buildNavItem(2, Iconsax.heart, 'Favorite'),
+            _buildNavItem(3, Iconsax.user, 'Profile'),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildNavItem(int index, IconData icon, String label) {
+    bool isSelected = _selectedIndex == index;
+
+    return GestureDetector(
+      onTap: () => _onItemTapped(index),
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+        padding: EdgeInsets.symmetric(
+          horizontal: isSelected ? 20 : 16,
+          vertical: 8,
+        ),
+        decoration: BoxDecoration(
+          color: isSelected ? AppColors.background.withOpacity(0.5) : Colors.transparent,
+          borderRadius: BorderRadius.circular(35),
+        ),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(
+              icon,
+              color: isSelected ? AppColors.primary : AppColors.primary.withOpacity(0.5),
+              size: 24,
+            ),
+            if (isSelected) ...[
+              const SizedBox(width: 8),
+              Text(
+                label,
+                style: TextStyle(
+                  fontFamily: "Poppins",
+                  color: AppColors.secondary,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          ],
+        ),
       ),
     );
   }
